7a885ba8a61a1e06f20e1119759e369fb86bdac2
Irw25-lschs <ianwhitaker2024@lschs.org>
2023-07-17 09:25:20 -0400

    Update sidebar.vue

--- vuetify-front-end-main/src/components/sidebar.vue
+++ vuetify-front-end-main/src/components/sidebar.vue
@@ -1,6 +1,6 @@
 <script>
 import {collapsed, toggleSidebar, sidebarWidth} from './state'
-import {showServersTable} from './open.vue'
+//import {showServersTable} from './open.vue'
     export default{
         props: {},
         setup () {
@@ -14,9 +14,9 @@ import {showServersTable} from './open.vue'
     <span class="collapse-icon" @click="toggleSidebar">
         <img class ="pic" src = "https://www.iotone.com/files/vendor/aegis-software_8.jpg">
     </span>
-    <span class="item" @click="showServersTable">
+  <!-- <span class="item" @click="showServersTable">
         <img class = "vmach" src="https://th.bing.com/th/id/R.de9f93de1dcf4db07c44f7c5d476f1ce?rik=0oESb6sMiR%2bntQ&riu=http%3a%2f%2ffindicons.com%2ffiles%2ficons%2f2711%2ffree_icons_for_windows8_metro%2f512%2fvirtual_machine.png&ehk=mvrMmkq1MxXm2OEahSa8s6d4xm9%2fwWxSfqfocTQOZEA%3d&risl=&pid=ImgRaw&r=0">
-    </span>
+    </span> -->
     </div>
 </template>
 
