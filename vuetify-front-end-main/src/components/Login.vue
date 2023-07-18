6469b66eccc0430636d706cda7e0da9d2619b8da
Stebaniel <113371484+Stebaniel@users.noreply.github.com>
2023-07-18 10:27:28 -0400

    Update Login.vue
    made a limit to how many times you can input username and password

--- vuetify-front-end-main/src/components/Login.vue
+++ vuetify-front-end-main/src/components/Login.vue
@@ -1,58 +1,74 @@
 <template>
-    <form name="login-form">
-      <div class="mb-3">
-        <label for="username">Username: </label>
-        <input type="text" id="username" v-model="input.username" />
-      </div>
-      <div class="mb-3">
-        <label for="password">Password: </label>
-        <input type="password" id="password" v-model="input.password" />
-      </div>
-      <button class="btn btn-outline-dark" type="submit" v-on:click.prevent="login()">
-        Login
-      </button>
+  <form name="login-form">
+    <div class="mb-3">
+      <label for="username">Username </label>
+      <input class='user' type="text" id="username" v-model="input.username" />
+    </div>
+    <div class="mb-3">
+      <label for="password">Password: </label>
+      <input class="pass" type="password" id="password" v-model="input.password" />
+    </div>
+    <button class="btn btn-outline-dark" type="submit" v-on:click.prevent="login()">
+      Login
+    </button>
 
-    </form>
-    <h3>Output: {{ this.output }}</h3>
-  
-  </template>
-  
-  <script>
-  import axios from 'axios';
+  </form>
+  <h3>{{ this.output }}</h3>
 
-  export default {
-    name: 'LoginView',
-    data() {
-      return {
-        input: {
-          username: "",
-          password: ""
-        },
-        output: "",
+</template>
+
+<script>
+import axios from 'axios';
+
+var strike = 1;
+var x = 0;
+
+export default {
+  name: 'LoginView',
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      },
+      output: "",
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        if(this.input.username == 'aiscorp.local' && this.input.password == 'Fahd'){
+          const response = await axios.post('http://strezza.aiscorp.local:4000/login', {
+            username: this.input.username,
+            password: this.input.password,
+          });
+          
+          const token = response.data.token;
+
+          x = 547893467;
+          //console.log(token);
+
+          //local storages
+          localStorage.setItem('brotha', x);
+          localStorage.setItem('jwt', token);
+
+          // Redirect or perform other actions after successful login
+          this.$router.push('/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJldXdpcmx3amVoZ2Z5a2FiayxjIiwicGFzc3dvcmQiOiJwYm9pZHRuamhlZ3ZkaGprZ2ZkIiwiaWF0IjoxNTE2MjM5MDIyfQ.zWXb9VM9Bxe-amtInmae7lJ7_1dx7pbfW3VONolyqsc')
+        } else {
+          this.output = 'Put in valid username & password';
+          if(strike == 3){
+            document.getElementsByClassName("user")[0].style.visibility = 'hidden';
+            document.getElementsByClassName("pass")[0].style.visibility = 'hidden';
+          } else {
+            strike = strike + 1;
+          } 
+        }
+      } catch (error) {
+        // Handle login error
+        //console.error('Error: ', error);
       }
     },
-    methods: {
-      async login() {
-        try {
-            if(this.input.username == 'aiscorp.local' && this.input.password == 'Fahd'){
-                const response = await axios.post('http://strezza.aiscorp.local:4000/login', {
-                    username: this.input.username,
-                    password: this.input.password,
-                });
 
-                const token = response.data.token;
-
-                //console.log(token);
-
-                // Store the JWT securely (e.g., in localStorage)
-                localStorage.setItem('jwt', token);
-                // Redirect or perform other actions after successful login
-                this.$router.push('/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJldXdpcmx3amVoZ2Z5a2FiayxjIiwicGFzc3dvcmQiOiJwYm9pZHRuamhlZ3ZkaGprZ2ZkIiwiaWF0IjoxNTE2MjM5MDIyfQ.zWXb9VM9Bxe-amtInmae7lJ7_1dx7pbfW3VONolyqsc')
-        }
-        } catch (error) {
-          // Handle login error
-          //console.error('Error: ', error);
-        }
-      },
-    },
-  }
+  },
+}
+</script>
