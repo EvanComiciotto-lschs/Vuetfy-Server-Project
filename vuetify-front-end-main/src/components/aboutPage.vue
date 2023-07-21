68cfb7c34ef6ff9ff66a9bbba4287440978c5520
epak7 <ethanpakuris2024@lschs.org>
2023-07-21 14:20:31 -0400

    Create aboutPage.vue

---
+++ vuetify-front-end-main/src/components/aboutPage.vue
@@ -0,0 +1,21 @@
+<template>
+<img id = "groupPhoto" src="https://i.ibb.co/fDTRWQD/AIS-Intern-Group-Photo.jpg">
+<img id = "flowchart" src="https://i.ibb.co/wrFLqF8/Microsoft-Teams-image.png">
+<p>2023 IT Asset Project</p>
+<p>Designed by La Salle College High School class of 2024 summer interns</p>
+<!--ABOUT PAGE CAN BE BUILT HERE
+    OR DESIGNED IN ANOTHER COMPONENT AND IMPORTED-->
+</template>
+
+<style scoped>
+#groupPhoto{
+    width: 20rem;
+    height: auto;
+    top: -1rem;
+    left: 2 rem;
+}
+#flowchart{
+    width: 20rem;
+    height: auto;
+}
+</style>
