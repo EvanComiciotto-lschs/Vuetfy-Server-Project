935b9d987d79a31e6bd3b9024fe914566212f46f
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2023-08-01 14:34:20 -0400

    Created Users File

---
+++ vuetify-front-end-main/src/components/Users.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    methods: {
+        resizeFlowChart() {
+            document.getElementById("flowchart").classList.toggle("big-chart");
+        },
+        resizePhoto() {
+            document.getElementById("groupPhoto").classList.toggle("big-photo");
+        }
+    }
+};
+</script>
+
+<template>
+   
+</template>
