977ca420dc1e91689a4604f162a3108e439f0101
McVillain <coreymcvillain@gmail.com>
2023-08-01 08:49:30 -0400

    Update backend.js
    Commented out an app.use line and fixed a reference error in the /servers method.

--- Backend-main/src/backend.js
+++ Backend-main/src/backend.js
@@ -6,7 +6,7 @@ var jwt = require('jsonwebtoken');
 const { json } = require('body-parser');
 var app = express();
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb', extended: true}));
 //declare the main lists so they can be accesssed between functions
@@ -18,7 +18,7 @@ let token = "6rqfduihfwsesuhgfweiouyw3rtfs897byw4tgoiuwy4sro9uw34t0u94t";
 app.post('/servers', function(req, res){
   var reqData = (req.body);   //store the request body
   if(req.headers.auth == token){
-    response.send("data received");
+    res.send("data received");
       reqData.Servers.forEach(function(server){
         //check if the VM is already in the list
       if (masterServerList.some(existingServer => existingServer.VMName === server.VMName)){
