0c8769d8440bf14c3e2881c7fa3a358df3f5ec85
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2023-08-03 13:06:18 -0400

    created Doughnut chart with mounted

--- vuetify-front-end-main/src/components/Users.vue
+++ vuetify-front-end-main/src/components/Users.vue
@@ -1,11 +1,67 @@
 <template>
-   <a href = "mailto:ecomiciotto@aiscorp.com">Get helps</a>
+  <div class="chart-container">
+    {{ chartData }}
+    <Doughnut id="my-chart-id" :options="chartOptions" :data="chartData" />
+  </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import {
+  Chart as ChartJS, ArcElement, Tooltip, Legend, ChartData
+} from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const chartOptions = {
+  responsive: true
+}
+const chartData = ref<ChartData<'doughnut'>>({
+  labels: ["hooray", "yipee"],
+  datasets: [{ data: [15,20], backgroundColor: ["#00ffff", "#ff0000"] }]
+})
+
+onMounted(async () => {
+  // let resp = await fetch('https://itassets.aiscorp.com:3000/messages', {
+  //   headers: {
+  //     auth: '6rqfduihfwsesuhgfweiouyw3rtfs897byw4tgoiuwy4sro9uw34t0u94t'
+  //   },
+  // });
+  let resp = {
+    json: async () => {
+      return {
+        departments: {
+          dept1: 5,
+          dept2: 10,
+        }
+      }
+    },
+    ok: true
+  };
+
+  if (resp.ok) {
+    let newData: ChartData<'doughnut'> = {
+      labels: [],
+      datasets: [{ data: [], backgroundColor: ["#00ffff", "#ff0000"] }]
+    }
+
+    const data = await resp.json();
+    console.log(chartData.value)
+    newData.labels = Object.keys(data.departments);
+    newData.datasets[0].data = Object.values(data.departments);
+    chartData.value = newData;
+  } else {
+    console.error('Error fetching data:');
+  }
+});
 </script>
 
-<style scoped>
 
+<style scoped>
+.chart-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+}
 </style>
