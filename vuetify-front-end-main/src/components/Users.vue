8fd542aa06144083a1d561a050b4f716d5b28b2f
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2023-08-02 22:54:43 -0400

    Added doughnut chart

--- vuetify-front-end-main/src/components/Users.vue
+++ vuetify-front-end-main/src/components/Users.vue
@@ -1,16 +1,52 @@
+<template>
+  <div class="chart-container">
+    <canvas ref="doughnutChartCanvas"></canvas>
+  </div>
+</template>
+
 <script>
+import { ref, onMounted } from 'vue';
+import { Doughnut } from 'chart.js';
+
 export default {
-    methods: {
-        resizeFlowChart() {
-            document.getElementById("flowchart").classList.toggle("big-chart");
-        },
-        resizePhoto() {
-            document.getElementById("groupPhoto").classList.toggle("big-photo");
-        }
-    }
+  name: 'MyDoughnutChart',
+  setup() {
+    const doughnutChartCanvas = ref(null);
+
+    onMounted(() => {
+      const ctx = doughnutChartCanvas.value.getContext('2d');
+      const data = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: 'My Dataset',
+            data: [300, 50, 100],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          },
+        ],
+      };
+      const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+      };
+
+      new Doughnut(ctx, {
+        data: data,
+        options: options,
+      });
+    });
+
+    return {
+      doughnutChartCanvas,
+    };
+  },
 };
 </script>
 
-<template>
-   <p>TESTING</p>
-</template>
+<style scoped>
+.chart-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
